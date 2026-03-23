45ff44288ebc62c3032a4f2d646c183b3e68deb0
rsirri <rajeshsirri@gmail.com>
2026-03-23 15:47:59 -0400

    UI5 Table with search and multi-field filter

--- webapp/controller/View1.controller.ts
+++ webapp/controller/View1.controller.ts
@@ -1,6 +1,12 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import MessageToast from "sap/m/MessageToast";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import SearchField from "sap/m/SearchField";
+import Table from "sap/m/Table";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import ListBinding from "sap/ui/model/ListBinding";
+import Event from "sap/ui/base/Event";
 // My first UI5 controller
 /**
  * @namespace helloui5.controller
@@ -10,17 +16,60 @@ export default class View1 extends Controller {
     public onInit(): void {
 
         // Create data - like filling internal table
-        const orderData = {
-            orderNumber: "45000012345",
-            customer: "1000456",
-            status: "Open",
-            netValue: 12500,
-            currency: "USD",
-            editable: false
-        };
+        // const orderData = {
+        //     orderNumber: "45000012345",
+        //     customer: "1000456",
+        //     status: "Open",
+        //     netValue: 12500,
+        //     currency: "USD",
+        //     editable: false
+        // };
 
         // Create JSON Model - like ABAP memory
-        const model = new JSONModel(orderData);
+        // const model = new JSONModel(orderData);
+
+        const ordersData = {
+            orders: [
+                {
+                    orderNumber: "4500012345",
+                customer: "1000456",
+                status: "Open",
+                netValue: 12500,
+                currency: "USD"
+            },
+            {
+                orderNumber: "4500012346",
+                customer: "1000789",
+                status: "In Process",
+                netValue: 8420,
+                currency: "USD"
+            },
+            {
+                orderNumber: "4500012347",
+                customer: "1000234",
+                status: "Open",
+                netValue: 15890,
+                currency: "EUR"
+            },
+            {
+                orderNumber: "4500012348",
+                customer: "1000678",
+                status: "Completed",
+                netValue: 3920,
+                currency: "USD"
+            },
+            {
+                orderNumber: "4500012349",
+                customer: "1000451",
+                status: "Open",
+                netValue: 21950,
+                currency: "EUR"
+            }
+        ]
+        };
+
+        // Create Model
+        const model = new JSONModel(ordersData);
 
         // Set model on the view - make it available to xml
         this.getView()?.setModel(model);
@@ -38,6 +87,41 @@ export default class View1 extends Controller {
         model.setProperty("/editable", !editable);
 
         MessageToast.show(editable ? "Display Mode" : "Edit Mode");
+    }
+
+    // FUNCTION for TABLE ON SEARCH
+    onSearch(event: Event): void {
+
+        // get search value
+        const query = (event.getSource() as SearchField).getValue();
+
+        // get table binding
+        const table = this.byId("ordersTable") as Table;
+        const binding = table.getBinding("items") as ListBinding;
+
+        // create filter
+        // const filters = query ? [new Filter("customer", FilterOperator.Contains, query)] : [];
+
+        // binding.filter(filters);
+
+        if (query) {
+            // Multiple filter - search across all fields
+            const filters = [
+                new Filter({
+                    filters: [
+                        new Filter("orderNumber", FilterOperator.Contains, query),
+                        new Filter("customer", FilterOperator.Contains, query),
+                        new Filter("status", FilterOperator.Contains, query),
+                        new Filter("currency", FilterOperator.Contains, query)
+                    ],
+                    and: false
+                })
+            ];
+            binding.filter(filters);
+        } else {
+            binding.filter([]);
+        }
+
     }
 
 }
